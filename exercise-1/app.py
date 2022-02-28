01338aeb116b4d8119b6490585a84d419bff044b
Tina Le <Tina.Le7617@gmail.com>
2022-02-27 20:10:43 -0500

    Adding exercise-1

--- exercise-1/app.py
+++ exercise-1/app.py
@@ -1,14 +1,10 @@
-from time import strftime
 from flask import Flask, render_template, request
+from time import strftime
 from datetime import datetime
 
 app = Flask(__name__)
 
-foods = []
-
-
 @app.get('/')
 def index():
     date = datetime.now()
-    # Saturday, February 12 2022 15:03
     return render_template('index.html', date = date.strftime('%A, %B %d %Y %H:%M:%S %p'))
